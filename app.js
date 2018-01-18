fa3005d6f4da02c2b8b6f6064e000600ab8d1d3c
Elio <elio.pereira.pro@gmail.com>
2018-01-18 12:36:39 +0100

    Ajout de commentaires et d'un debut d'implementation

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var http = require('http')
 var url = require('url')
 const { exec } = require('child_process')
 var base64 = require('base-64')
+var AsyncLock = require('async-lock')
+var lock = new AsyncLock();
+static var idlist = new ArrayList;
+static var id = 0;
+static var idurl = (int,String);
 
 //Moteur de template
 app.set('view engine','ejs')
@@ -29,10 +34,11 @@ app.get('/', (request, response) =>{
   //response.render('/pages/index')
 })
 
-//renvoie les paramètres de l'outil
+//renvoie les paramètres de l'outil (juste un exemple test pour le moment)
 app.get('/outil_list', (request, response) =>{
   fs.readFile('/home/primary/service/service_server/ListeOutilParam/Outilname.js','utf8', (err, data)=>{
-  /*fs.readFile('path sur la VM', 'utf8', (err, data) =>{*/
+  /*Il faudra mettre le vrai path à un moment aussi
+  fs.readFile('path sur la VM', 'utf8', (err, data) =>{*/
     if(err) throw err;
     else {
       try{
@@ -46,20 +52,21 @@ app.get('/outil_list', (request, response) =>{
     }
   })
 })
-
+/**/
 app.post('/param', jsonParser, (request, response) =>{
   if(!request.body) return response.end("Erreur dans le paramétrage")
+  //on reçoit les paramètres en format JSON, il faut maintenant pouvoir l'exploiter pour le script
   var param = request.body
-  var test = toType(param)
-  console.log(test)
   console.log(param)
-  const paramrecu = JSON.stringify(param)
+  const paramrecu = JSON.parse(param)
   console.log(paramrecu);
+
   /*TODO : recevoir/lire les paramètres correctement
-  //si url =/= null alors on envoie vers extractor url
-  //sinon on envoie vers un file qu'on aura téléchargé déjà */
-  exec('sudo bash /home/primary/service/service_server/script_adress_auto.sh ' + request.body, (error, stdout, stderr) =>{
-//exec('bash path VM /script.sh', (error, stdout, stderr)
+  *si url -> script_adress_auto sinon script
+  *comment répurer l'url et le nom du fichier?
+  *envoyer directement vers script_adress_auto
+  */
+  exec('sudo bash /home/primary/service/service_server/script_adress_auto.sh ', (error, stdout, stderr) =>{
     if (error) {
       console.error(`exec error: ${error}`);
       return;
@@ -67,21 +74,47 @@ app.post('/param', jsonParser, (request, response) =>{
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
   }
+}
   //renvoyer une autre requête POST à la fin?
   response.end("Paramètres bien reçus : " + paramrecu)
 })
 
-
-app.post('/extractor', rawParser, (request, response) =>{
+/**/
+app.post('/extractorVID', rawParser, (request, response) =>{
 if(!request.body) { return response.end("Erreur, pas de données") }
+  //on veut protéger id d'une éventuelle erreur de synchro
+  lock.acquire(id, function(done){
+  id = id++;
+  idurl = (id,null);
+  idlist.add(idurl);
+  done(err,ret);
+}, function(err,ret){
+});
+  //on decode le base64
   var rawParsed = request.body
   var test = toType(rawParsed)
   console.log(test)
   var decodedData = base64.decode(rawParsed)
   console.log(decodedData)
-  //créer un file avec les données dessus avec le nom qu'il faut.
+  //créer un file avec les datas
   response.end("Got a POST request")
 })
+
+/*
+*/
+app.post('/extractorURL', urlParser, (request, response) =>{
+  if(!request.body) { return response.end("Erreur avec l'url") }
+  var urlreceived = request.body
+  //on veut protéger id d'une éventuelle erreur de synchro
+  lock.acquire(id, function(done){
+  id = id++;
+  idurl = (id,request.body);
+  idlist.add(idurl);
+  done(err,ret);
+}, function(err,ret){
+});
+
+}
 
 app.put('/', (request, response) =>{
   response.end("Got a PUT request")
