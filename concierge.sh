8aa13145636ab2fb7e575835917116184dce698b
Thomas Cusson <poudingue@hotmail.fr>
2018-03-14 15:43:16 +0100

    radical concierge works ! Delete all projects named in «deletable_projects»

--- concierge.sh
+++ concierge.sh
@@ -1,10 +1,8 @@
-#concierge
-#Encore à déterminer : Prendre des arguments ?
-rm *.mp4
-rm *.avi
-rm *.webm
-#Les fichiers téléchargés si ils sont à la source. On mettra sans doute dans un fichier
-cd videos
-#fichier stockant les dates ?
-#récupérer dates plus lointaines
-rm datelointaine
+#!/bin/sh
+
+for filename in `ls deletable_projects`
+do
+    rm -rf "projects/$filename"
+done
+# Vider la liste des projets "supprimables"
+rm -f "deletable_projects/*"
